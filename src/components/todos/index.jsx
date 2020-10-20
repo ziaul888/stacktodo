ac6f57b2475b8bae063c99d3fe2820686db5d856
unknown <ziaul935@gmail.com>
2020-10-21 02:03:08 +0600

    start projcet

---
+++ src/components/todos/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return (
+            <div className="dispaly-4 text-center mb-2">
+                <h1> Stack todos</h1>
+            </div>
+        );
+    }
+}
+
+export default index;
